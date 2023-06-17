d9f6b1983482b9e38cbd777e5245e62640e16db7
vrena4567 <v.rena4567@gmail.com>
2023-06-17 06:38:04 +0200

    add square

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,21 @@ public class Main {
         Shape kor = new Circle(2);
         System.out.println("A kerulete: " + kor.getPerimeter());
         System.out.println("A terulete: " + kor.getArea());
+
+        Shape shape = new Shape() {
+            @Override
+            public double getPerimeter() {
+                return 0;
+            }
+
+            @Override
+            public double getArea() {
+                return 0;
+            }
+        };
+
+        Shape negyzet = new Square(2);
+        System.out.println("A kerulete: " + negyzet.getPerimeter());
+        System.out.println("A terulete: " + negyzet.getArea());
     }
 }
---
+++ src/Square.java
@@ -0,0 +1,16 @@
+public class Square implements Shape{
+    private double side;
+
+    public Square(double side) {
+        this.side = side;
+    }
+    @Override
+    public double getPerimeter() {
+        return 4*side;
+    }
+
+    @Override
+    public double getArea() {
+        return side*side;
+    }
+}
